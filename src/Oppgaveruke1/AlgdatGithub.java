268f80a208eb9eb5bc6b8335df036fee2c235e89
jonhagos <jon4geo@gmail.com>
2020-08-30 09:45:47 +0200

    oppdatert petra

--- src/Oppgaveruke1/AlgdatGithub.java
+++ src/Oppgaveruke1/AlgdatGithub.java
@@ -3,7 +3,7 @@ package Oppgaveruke1;
 public class AlgdatGithub {
 
     public static void main(String[] args) {
-        
+
         System.out.println("petra can telle til 20");
         for(int i = 11;i <= 20;i++){
             System.out.print(i +",");
